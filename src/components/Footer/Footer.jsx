3514a6ce568a8271f73dfe9064edecb76c06849f
andrejarboe <andrejarboe@gmail.com>
2018-10-14 14:14:24 -0400

    add 404

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,33 +7,28 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-          <div className="footer-container">
-          
+        <div className="footer-container">
           <div className="footer-logo">
             <h2>Jingles</h2>
             <p>Your slogan here.</p>
           </div>
-        <div className="site-map">
-          <h3>Site Map</h3>
-          <Link to="/">Products</Link>
-          <Link to="/">About</Link>
-          <Link to="/">FAQ</Link>
-          <Link to="/contact">
-            Request a quote
-          </Link>
+          <div className="site-map">
+            <h3>Site Map</h3>
+            <Link to="/">Products</Link>
+            <Link to="/">About</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/contact">Request a quote</Link>
+          </div>
+          <div className="contact">
+            <h3>Contact</h3>
+            <p>contact@jingles.com</p>
+          </div>
         </div>
-        <div className="contact">
-          <h3>Contact</h3>
-          <p>contact@jingles.com</p>
-
-        </div>
-        <div className="copy-right">
-        <a href="https://www.jarboestudios.com/" target="_blank">
-            Designed and coded at Jarboe Studios
-          </a>
-          <p>All Right Reserves &copy; 2018</p>
-        </div>
-
+          <div className="copy-right container">
+            <a href="https://www.jarboestudios.com/" target="_blank">
+              Designed and coded at Jarboe Studios
+            </a>
+            <p>All Right Reserves &copy; 2018</p>
           </div>
       </footer>
     );
---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Layout from "../components/layout";
+
+
+export default class NotFoundPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>404</h1>
+      </Layout>
+    )
+  }
+}
